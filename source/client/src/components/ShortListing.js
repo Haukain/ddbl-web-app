61a9b637232b3c1be27e14b4beec538d17822ad2
Haukain <Haukain@users.noreply.github.com>
2019-10-24 10:59:32 +0200

    Drag n' drop implementation
    First try of dnd implementation for the shortlisting phase

--- source/client/src/components/ShortListing.js
+++ source/client/src/components/ShortListing.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+<<<<<<< HEAD
 import ShortListingBoard from './ShortListingBoard'
 import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   board : {
     padding : theme.spacing(3)
+=======
+import Container from '@material-ui/core/Container';
+import ShortListingBoard from './ShortListingBoard'
+
+const useStyles = makeStyles(theme => ({
+  root : {
+  },
+  test : {
+    margin : theme.spacing(20,0,20,0),
+>>>>>>> Drag n' drop implementation
   }
 }));
 
@@ -13,8 +24,13 @@ export default function ShortListing(props) {
   const classes = useStyles();
 
   return (
+<<<<<<< HEAD
     <Container>
       <div className={classes.board}>
+=======
+    <Container className={classes.root}>
+      <div className={classes.test}>
+>>>>>>> Drag n' drop implementation
         <ShortListingBoard/>
       </div>
     </Container>
