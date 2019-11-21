4ff052650d641d54cd973ae9b52e290b3eb6c2f1
Haukain <Haukain@users.noreply.github.com>
2019-11-21 22:26:56 +0100

    update homepage UI

--- source/client/src/components/HomePage.js
+++ source/client/src/components/HomePage.js
@@ -1,54 +1,80 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Link as RouterLink } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white
-    }
+const styles = theme => ({
+  root: {
+    height: '100vh',
   },
-  button: {
-    margin: theme.spacing(3)
+  image: {
+    backgroundImage: 'url(/content/images/homepage.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: theme.palette.grey[50],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   }
-}));
+});
 
-export default function SignIn(props) {
-  const classes = useStyles();
+class HomePage extends React.Component{
+  constructor(props){
+    super(props)
 
-  return (
-    <Container component='main' maxWidth='xs'>
-      <Grid container>
-        <Grid item xs>
-          <Button
-            component={RouterLink}
-            to='/signup'
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.button}
-          >
-            Sign Up
-          </Button>
-        </Grid>
-        <Grid item xs>
-          <Button
-            component={RouterLink}
-            to='/signin'
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.button}
-          >
-            Sign In
-          </Button>
+    this.switchForm = this.switchForm.bind(this)
+    this.generateSignUp = this.generateSignUp.bind(this)
+    this.generateSignIn = this.generateSignIn.bind(this)
+    
+    this.state = {
+      form : this.generateSignUp
+    }
+  }
+
+  switchForm() {
+    this.setState({form:this.state.form===this.generateSignUp?this.generateSignIn:this.generateSignUp})
+  }
+
+  generateSignUp() {
+    return <SignUp switchForm={this.switchForm}/>
+  }
+
+  generateSignIn() {
+    return <SignIn switchForm={this.switchForm}/>
+  }
+
+  copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  render(){
+    const { classes } = this.props;
+
+    return (
+      <Grid container component="main" className={classes.root}>
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          {this.state.form()}  
+          <Box mt={5}>
+            {this.copyright()}
+          </Box>
         </Grid>
       </Grid>
-    </Container>
-  );
+    );
+  }
+  
 }
+
+export default withStyles(styles, { withTheme: true })(HomePage);
