01edafb85d6aab3ea2e0bac0fc6971c6110e52da
Timothy Cabaret <Haukain@users.noreply.github.com>
2019-12-17 14:12:41 +0100

    Various app improvements (#16)
    * longist batch check
    
    * add More info text to the step selection page
    
    * edit KPI name feature (WIP)
    
    * add cancel edition Kpi
    
    * update kpi edition

--- source/client/src/components/LonglistStep/LonglistBoard.js
+++ source/client/src/components/LonglistStep/LonglistBoard.js
@@ -7,7 +7,10 @@ import {
   ListItemText,
   ListItemIcon,
   Checkbox,
-  Typography
+  Typography,
+  ListSubheader,
+  FormControlLabel,
+  TextField
 } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import update from 'immutability-helper';
@@ -31,9 +34,13 @@ class LonglistBoard extends React.Component {
     super(props);
 
     this.state = {
-      kpiList: []
+      kpiList: [],
+      batchCheck: true,
     };
     this.handleChecked = this.handleChecked.bind(this);
+    this.handleBatchCheck = this.handleBatchCheck.bind(this);
+    this.handleKpiToggleEdition = this.handleKpiToggleEdition.bind(this);
+    this.handleValidateKpiEdition = this.handleValidateKpiEdition.bind(this);
     this.saveHandler = this.saveHandler.bind(this);
     this.importKpis = this.importKpis.bind(this);
     this.addKpis = this.addKpis.bind(this);
@@ -48,13 +55,53 @@ class LonglistBoard extends React.Component {
     });
   }
 
+  handleBatchCheck() {
+    let newState = !this.state.batchCheck
+    this.setState({batchCheck:newState})
+    let kpiList = this.state.kpiList
+    for(let k of kpiList){
+      k.isChecked = newState
+    }
+    this.setState({kpiList:kpiList})
+  }
+
+  handleKpiToggleEdition(id){
+    this.setState({
+      kpiList: update(this.state.kpiList, {
+        [id]: { isBeingEdited: { $set: true } }
+      })
+    });
+  }
+
+  handleKpiEdition(event,id){
+    this.setState({
+      kpiList: update(this.state.kpiList, {
+        [id]: { name : { $set: event.target.value } }
+      })
+    });
+  }
+
+  handleEnterValidateKpiEdition(event,id) {
+    if(event.key==='Enter'){
+      this.handleValidateKpiEdition(id)
+    }
+  }
+
+  handleValidateKpiEdition(id){
+    this.setState({
+      kpiList: update(this.state.kpiList, {
+        [id]: { isBeingEdited: { $set: false } }
+      })
+    });
+  }
+
   generateKpiList() {
     let kpiList = [];
     // eslint-disable-next-line
     for (let [i, k] of this.state.kpiList.entries()) {
       kpiList.push(
         <ListItem key={i}>
-          <ListItemText primary={k.name} />
+          {k.isBeingEdited?<TextField autoFocus value={this.state.kpiList[i].name} onKeyDown={(e) => this.handleEnterValidateKpiEdition(e,i)} onChange={(e) => this.handleKpiEdition(e,i)} onBlur={() => this.handleValidateKpiEdition(i)}/>:<ListItemText primary={k.name} onDoubleClick={() => this.handleKpiToggleEdition(i)}/>}
           <ListItemIcon>
             <Checkbox
               disableRipple
@@ -95,13 +142,13 @@ class LonglistBoard extends React.Component {
   importKpis(list) {
     this.setState({
       kpiList: update(this.state.kpiList, {
-        $push: list.map(e => ({ name: e, isChecked: true }))
+        $push: list.map(e => ({ name: e, isChecked: this.state.batchCheck, isBeingEdited : false }))
       })
     });
   }
 
   addKpis(name) {
-    let newKpi = { name: name, isChecked: true };
+    let newKpi = { name: name, isChecked: this.state.batchCheck, isBeingEdited : false };
     this.setState({ kpiList: update(this.state.kpiList, { $push: [newKpi] }) });
   }
 
@@ -123,7 +170,18 @@ class LonglistBoard extends React.Component {
             <LonglistManualImport addKpis={this.addKpis} />
           </Grid>
           <Grid item xs={4}>
-            <List className={classes.list}>{this.generateKpiList()}</List>
+            <List className={classes.list}>
+              <ListSubheader
+              disableSticky
+              >
+                <FormControlLabel
+                  control={<Checkbox color="primary" checked={this.state.batchCheck} onClick={this.handleBatchCheck}/>}
+                  label={'Check All'}
+                  labelPlacement="start"
+                />
+              </ListSubheader>
+              {this.generateKpiList()}
+            </List>
             <Button
               variant='contained'
               disabled={this.state.kpiList.length === 0}
--- source/client/src/components/StepSelectionPage.js
+++ source/client/src/components/StepSelectionPage.js
@@ -9,7 +9,10 @@ import {
   CardContent,
   CardMedia,
   CardActions,
-  CardActionArea
+  CardActionArea,
+  Dialog,
+  DialogTitle,
+  DialogContent
 } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -30,8 +33,43 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function SignIn(props) {
+const summaries = [
+  {
+      "title" : "Longlist",
+      "summary" : (<div><Typography gutterBottom align={'justify'}>For the <b>Longlisting step</b> it is essential to list every single KPI of every single department within your organization.</Typography>
+      <Typography gutterBottom align={'justify'}>Later stages will focus on filtering these KPIs but in this step we will only be focusing on gathering every KPI in the organization.</Typography></div>)
+  },
+  {
+      "title" : "Shortlist",
+      "summary" : (<div><Typography gutterBottom align={'justify'}>In the <b>Shortlisting step</b> we will be focusing on bringing the number of KPIs down to a manageable amount by looking into the importance and ease of measure of every single KPI.</Typography>
+      <Typography gutterBottom align={'justify'}>By sorting the KPIs based on the importance and ease of measure we will bring down the total number of KPIs, this will lead to less clutter of KPI management and the gathering of the information to calculate the KPIs.</Typography></div>)
+  },
+  {
+      "title" : "Definition" ,
+      "summary" : (<div><Typography gutterBottom align={'justify'}>In the <b>Definition step</b> we will be gathering the meta-data behind the KPIs such as who is responsible for the KPI and what does the KPI cost to measure it?</Typography>
+      <Typography gutterBottom align={'justify'}>This will give more insight and maybe lead to not measuring the KPI since it cost simply too much or isn’t that important after all.</Typography>
+      <Typography gutterBottom align={'justify'}>By defining the KPIs you will get more insight in the KPIs that are being measured.</Typography>
+      <Typography gutterBottom align={'justify'}>The visuals of the <b>visualization panel</b> will provide you with information about the meta-data behind the KPIs.</Typography>
+      <Typography gutterBottom align={'justify'}>This will give you insight on who is responsible for the most KPIs or what the most occurring problem is with the current list of KPIs.</Typography>
+      <Typography gutterBottom align={'justify'}>Based on this information you can make it easier to measure the current list of KPIs.</Typography>
+      <Typography gutterBottom align={'justify'}>This gives more freedom to do other tasks within the company.</Typography></div>)
+  }
+]
+
+export default function StepSelectionPage(props) {
   const classes = useStyles();
+
+  const [dialog,setDialog] = React.useState(false)
+  const [info,setInfo] = React.useState({title:'',summary:''})
+
+  function openDialog(i) {
+    setInfo(summaries[i])
+    setDialog(true)
+  }
+
+  function closeDialog() {
+    setDialog(false)
+  }
 
   return (
     <Container>
@@ -54,7 +92,7 @@ export default function SignIn(props) {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size='small' color='primary'>
+              <Button size='small' color='primary' onClick={() => openDialog(0)}>
                 More info
               </Button>
             </CardActions>
@@ -79,7 +117,7 @@ export default function SignIn(props) {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size='small' color='primary'>
+              <Button size='small' color='primary' onClick={() => openDialog(1)}>
                 More info
               </Button>
             </CardActions>
@@ -103,13 +141,21 @@ export default function SignIn(props) {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size='small' color='primary'>
+              <Button size='small' color='primary' onClick={() => openDialog(2)}>
                 More info
               </Button>
             </CardActions>
           </Card>
         </Grid>
       </Grid>
+      <Dialog onClose={closeDialog} aria-labelledby="customized-dialog-title" open={dialog}>
+        <DialogTitle id="customized-dialog-title" onClose={closeDialog}>
+          {info.title}
+        </DialogTitle>
+        <DialogContent dividers>
+          {info.summary}
+        </DialogContent>
+      </Dialog>
     </Container>
   );
 }
